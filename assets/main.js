5f77bf3746b7a05e68fa6d92e12d38c7fffbd7bd
DamianRondinon <95187328+DamianRondinon@users.noreply.github.com>
2022-08-23 22:29:44 -0300

    Ejercicio crypto = primera parte js finalizada(requestJS)

---
+++ assets/main.js
@@ -0,0 +1,19 @@
+const tbody = document.querySelector('tbody');
+const preBTN = document.querySelector('.left');
+const nextBTN = document.querySelector('.right');
+const pageNumber = document.querySelector('.page-number');
+const form = document.querySelector('.form')
+const searchInput = document.querySelector('.input-search');
+const top100 = document.querySelector('.top-100');
+const btnContainer = document.querySelector('.pagination');
+
+const loadCoins = 
+
+
+
+
+
+const init = () => {
+window.addEventListener('DOMContentLoaded', loadCoins);
+};
+init();
