cbcbd9326415ddadb8d79513f4096fe49cb6213a
DamianRondinon <95187328+DamianRondinon@users.noreply.github.com>
2022-08-24 22:37:07 -0300

    Proyecto de clase: finalizado

--- assets/main.js
+++ assets/main.js
@@ -1,19 +1,156 @@
-const tbody = document.querySelector('tbody');
-const preBTN = document.querySelector('.left');
-const nextBTN = document.querySelector('.right');
-const pageNumber = document.querySelector('.page-number');
-const form = document.querySelector('.form')
-const searchInput = document.querySelector('.input-search');
-const top100 = document.querySelector('.top-100');
-const btnContainer = document.querySelector('.pagination');
+const tbody = document.querySelector("tbody");
+const prevBTN = document.querySelector(".left");
+const nextBTN = document.querySelector(".right");
+const pageNumber = document.querySelector(".page-number");
+const form = document.querySelector(".form");
+const searchInput = document.querySelector(".input-search");
+const top100 = document.querySelector(".top-btn");
+const btnContainer = document.querySelector(".pagination");
 
-const loadCoins = 
+const formatearNumeros = num => {
+  return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g,'.');
+};
+
+const nFormatter = (num, digits) => {
+  const symbol = [
+    { value: 1, symbol: ''},
+    { value: 1e3, symbol: 'K'},
+    { value: 1e6, symbol: 'M'},
+    { value: 1e9, symbol: 'MM'},
+  ]
+
+  const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
+  let item = symbol
+  .slice()
+  .reverse()
+  .find(item => {
+    return (num >= item.value);
+  });
+
+  return item
+    ? (num / item.value).toFixed(digits).replace(rx, '$1') + item.symbol
+    : '0';
+  };
+
+const renderCoin = (coin) => {
+  const {
+    rank,
+    market_cap_usd,
+    name,
+    symbol,
+    price_usd,
+    volume24,
+    percent_change_24h,
+    percent_change_7d,
+  } = coin;
+  return `
+    <tr>
+    <td>#${rank}</td>
+    <td class="coin-title">${name} (${symbol.toUpperCase()})</td>
+    <td>$${formatearNumeros(price_usd)}</td>
+    <td>$${nFormatter(market_cap_usd)}</td>
+    <td>$${nFormatter(volume24)}</td>
+    <td><span class="${
+      percent_change_24h < 0 ? "down" : "up"
+    }">${percent_change_24h}</span></td>
+    <td><span class="${
+      percent_change_7d < 0 ? "down" : "up"
+    }">${percent_change_7d}</span></td>
+</tr>
+    `;
+};
+
+const renderCoins = (coins, current) => {
+  if (!coins.length) {
+    tbody.innerHTML = `<h1>No se encontraron resultados, realice una nueva busqueda ...</h1>`;
+    top100.classList.remove("hidden");
+    btnContainer.classList.add("hidden");
+    form.reset();
+    return;
+  }
+  btnContainer.classList.remove("hidden");
+  tbody.innerHTML = coins[current].map(renderCoin).join("");
+};
+
+const disablePreviousBTN = prev => {
+  if (prev == null) {
+    prevBTN.classList.add("disabled");
+  } else {
+    prevBTN.classList.remove("disabled");
+  }
+};
+
+const disableNextBTN = (next, total) => {
+  if(next == total){
+    nextBTN.classList.add('disabled');
+  } else {
+    nextBTN.classList.remove('disabled');
+  }
+};
+  
+const resetCount = (coins) => {
+  prev = coins.prev;
+  current = coins.current;
+  next = coins.next;
+  total = coins.total;
+  results = coins.results;
+};
+
+const loadCoins = async (e) => {
+  e.preventDefault();
+  const searchedValue = searchInput.value.trim();
+  let coins = await requestCoins(searchedValue);
+  resetCount(coins);
+
+  if (searchedValue) {
+    top100.classList.remove("hidden");
+    form.reset();
+  } else {
+    top100.classList.add("hidden");
+  }
+
+  pageNumber.innerHTML = current + 1;
+  renderCoins(results, current);
+  disablePreviousBTN(prev);
+  disableNextBTN(next, total);
 
 
+nextBTN.addEventListener('click', e => {
+  e.stopImmediatePropagation();
+  if (next == total) return;
+  prev = current;
+  current = next;
+  next += 1;
+
+  renderCoins(results, current);
+  pageNumber.innerHTML = current + 1;
+  disablePreviousBTN(prev);
+  disableNextBTN(next, total);
+});
+
+prevBTN.addEventListener('click', e => {
+  e.stopImmediatePropagation();
+  if(prev == null) return;
+  current -= 1;
+  prev = prev == 0 ? null : prev - 1;
+  next -= 1;
+  renderCoins(results, current);
+  pageNumber.innerHTML = current + 1;
+
+  disablePreviousBTN(prev);
+  disableNextBTN(next, total);
+});
+};
+
+const showTop100 = () => {
+  loadCoins();
+};
 
 
 
 const init = () => {
-window.addEventListener('DOMContentLoaded', loadCoins);
+  window.addEventListener("DOMContentLoaded", loadCoins);
+  form.addEventListener("submit", loadCoins);
+  top100.addEventListener('click', loadCoins);
 };
 init();
--- assets/request.js
+++ assets/request.js
@@ -1,32 +1,34 @@
-const requestCoins = async value => {
-    const baseURL = 'https://api.coinlore.net/api/tickers/?start=0&limit=100'
 
-    const response = await fetch(baseURL);
-    const json = await response.json();
-    // const data = json.data;
-    const results = value
-    ? chunkArrayInGroups(data.filter(coin => coin.name.toLowerCase().includes
-    (value.toLowerCase())
-    ),10
-    )
+const requestCoins = async (value) => {
+  const baseURL = "https://api.coinlore.net/api/tickers/?start=0&limit=100";
+
+  const response = await fetch(baseURL);
+  const json = await response.json();
+  const data = json.data;
+
+  const results = value
+    ? chunkArrayInGroups(
+        data.filter((coin) =>
+          coin.name.toLowerCase().includes(value.toLowerCase())
+        ),
+        10
+      )
     : chunkArrayInGroups(data, 10);
 
-    return {
-        results: results,
-        total: results.length,
-        current: 0,
-        prev: null,
-        next: 1,
-
-    };
-
+  return {
+    results: results,
+    total: results.length,
+    current: 0,
+    prev: null,
+    next: 1,
+  };
 };
 
-    const chunkArrayInGroups = (arr, size) => {
-        let chunk = [];
-        for(let i = 0; i < arr.lenght; i + size) {
-            chunk.push(arr.slice(i, i + size));
-        }
-        return chunk;
-    };
-
+const chunkArrayInGroups = (arr, size) => {
+  let chunk = [];
+  for (let i = 0; i < arr.length; i += size) {
+    chunk.push(arr.slice(i, i + size));
+  }
+  console.log(chunk);
+  return chunk;
+};
